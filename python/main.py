9d6441faaf0baa4d8e2fc530bec70ef8f28fda96
Masoud <masuodmohararzadeh@gmail.com>
2025-10-22 15:52:07 +0330

    HTTP Streams - 8 Bytes

--- python/main.py
+++ python/main.py
@@ -1,1 +1,13 @@
-print("I hope I get the job!")
+f = open("messages.txt")
+with open("messages.txt", "rb"):
+    data = f.read(8)
+    while data:
+        print("read:", data)
+        data = f.read(8)
+
+with open("messages.txt", "r", encoding="utf-8") as f:
+    print(type(f))
+    bytes = f.read(8)
+    while bytes:
+        print("read:", bytes)
+        bytes = f.read(8)
