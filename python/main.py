21d90946d0dbc64355fce9bbc636f6c4767383f2
Masoud <masuodmohararzadeh@gmail.com>
2025-10-23 03:32:05 +0330

    fix: python implementation didn't read file in binary

--- python/main.py
+++ python/main.py
@@ -1,13 +1,10 @@
-f = open("messages.txt")
-with open("messages.txt", "rb"):
+# os.Open() in golang open file in binary
+# read the file in binary mode
+with open("messages.txt", "rb") as f:
     data = f.read(8)
     while data:
-        print("read:", data)
+        # convert the binary to str
+        # errors="replace" tells if you can't decode properly don't crash, just replace them with replacement character
+        # emoji's can cause problem, they are 4 byte, so when you decode them, it may cause incomplete byte sequence
+        print("read:", data.decode("utf-8", errors="replace"))
         data = f.read(8)
-
-with open("messages.txt", "r", encoding="utf-8") as f:
-    print(type(f))
-    bytes = f.read(8)
-    while bytes:
-        print("read:", bytes)
-        bytes = f.read(8)
